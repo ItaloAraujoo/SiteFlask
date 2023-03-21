7b055fa04d4cf459912b16f6ad082f47e6496b85
ItaloAraujoo <italo3040@gmail.com>
2023-03-21 17:48:54 -0300

    adicionando formularios no html, aplicando bootstrap nos form e liberando metodo post das paginas

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
+
 app = Flask(__name__)
 
 lista_usuarios = ['Italo', 'Amanda', 'Gabriel', 'Alessandra']
+
+# segurança dos formulários do site
+app.config['SECRET_KEY'] = '9c0710c20aaaaa8adb5eec94652d8c0c'
 
 
 @app.route('/')
@@ -16,9 +21,11 @@ def contato():
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 if __name__ == '__main__':
     app.run(debug=True)
